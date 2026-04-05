ce83bfcb208fa645e72daf17bfd437742eb4195b
RodolfoAugustoAguiar <268475948+RodolfoAugustoAguiar@users.noreply.github.com>
2026-04-05 01:00:57 -0300

    feat: versão inicial (app.py e README).

---
+++ README.md
@@ -0,0 +1,8 @@
+# Menu do laboratório e do suporte técnico
+
+Este projeto em Python foi cirado para praticar estrutura **match** e **case**.
+
+O programa solicita a inserção de uma das opções listadas para o uso do laboratório, retornando com informações sobre o item escolhido. Também retorna ao status do pedido de manutenção técnica.
+
+
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=white)
---
+++ app.py
@@ -0,0 +1,39 @@
+# Menu do Laboratório
+# Autor: Rodolfo Augusto Aguiar
+
+import os
+
+os.system('cls')
+
+print ("Menu do Laboratório")
+print("Opções: ")
+print("1 - Listar computadores disponíveis")
+print("2 - Abrir chamado de manutenção")
+print("3 - Ver regras de uso do laboratório")
+
+opcao1 = input("Digite uma opção (1 - PCs, 2 - Manutenção, 3 - Regras): ")
+
+match opcao1:
+    case "1" | "um" | "Um" | "UM":
+        print("Computadores disponíveis: Lab01-PC01 ao Lab01-PC20.")
+    case "2" | "dois" | "Dois" | "DOIS":
+        print("Abrindo chamado de manutenção... descreva o problema.")
+        print("Menu de Suporte Técnico")
+        print("Opções: ")
+        print("P: Pedido pendente")
+        print("E: Pedido enviado")
+        print("C: Pedido cancelado")
+        opcao2 = input("Digite a opção (P, E ou C): ").upper()
+        match opcao2:
+            case "P":
+                print("Seu pedido está Pendente e em processamento.")
+            case "E":
+                print("Seu pedido já foi Enviado! Verifique seu e-mail.")
+            case "C":
+                print("Seu pedido foi Cancelado. Entre em contato com o RH.")
+            case _:
+                print("Opção inválida. Tente novamente.")
+    case "3" | "três" | "Três" | "TRêS":
+        print("Regras: uso individual, silêncio e não alterar configurações.")
+    case _:
+        print("Opção inválida. Tente novamente.")
